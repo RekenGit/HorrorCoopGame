b6c5b07838dd837c2152b31e2970d6386639ec9c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 23:52:31 +0200

    Revert "Revert "Horror game scripts""
    This reverts commit bf103137079a136e1f29b4f2a43ef9dc79123fe4.

---
+++ Assets/Script/UI/UIOptions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOptions : MonoBehaviour
+{
+    public GameObject keyboardAndMouse;
+    public GameObject graphic;
+    public GameObject sounds;
+    public GameObject home;
+
+    #region OpenOptionsTab
+    public void OpenOptionKeyboardAndMouse()
+    {
+        home.SetActive(false);
+        keyboardAndMouse.SetActive(true);
+    }
+    public void OpenOptionGraphic()
+    {
+        home.SetActive(false);
+        graphic.SetActive(true);
+    }
+    public void OpenOptionSounds()
+    {
+        home.SetActive(false);
+        sounds.SetActive(true);
+    }
+    public void OpenOptionHome()
+    {
+        keyboardAndMouse.SetActive(false);
+        graphic.SetActive(false);
+        sounds.SetActive(false);
+        home.SetActive(true);
+    }
+    #endregion
+}
